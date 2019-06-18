4592ddd67ac7e83a678d5a55444f29d224d10b19
Dananjaya Ramanayake <dananjaya86@yahoo.com>
2019-06-18 14:55:48 +0200

    logout feature

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -86,7 +86,8 @@ class AppAuthenticator extends AbstractFormLoginAuthenticator
         }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('homepage.index'));
     }
 
     protected function getLoginUrl()
